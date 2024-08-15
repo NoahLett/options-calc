dd10093e20cb55fdeb4bfcc975a48f17b33fbd79
Noah <noahblett96@gmail.com>
2024-08-15 10:48:12 -0700

    Completed initial setup and installs

--- src/server/main.js
+++ src/server/main.js
@@ -1,12 +1,21 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
 
+const PORT = process.env.PORT;
 const app = express();
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.get("/hello", (req, res) => {
   res.send("Hello Vite + React!");
 });
 
-ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000..."),
+ViteExpress.listen(app, PORT, () =>
+  console.log(`Server is listening on port ${PORT}...`),
 );
