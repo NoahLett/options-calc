865fd0ff618d66a3d9b5fb65460e0edad40920b1
Noah <noahblett96@gmail.com>
2024-08-20 14:03:12 -0700

    simplified some logic

--- src/client/Components/ViewButtons.jsx
+++ src/client/Components/ViewButtons.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 
-const ViewButtons = ({ selectedTransactionIds, selectedOptionIds, showTransactionForm, showOptionForm, allowed }) => {
+const ViewButtons = ({ showTransactionForm, showOptionForm, allowed }) => {
   return (
     <div className="flex justify-around my-4">
       <button
         className={`text-white py-2 px-3 rounded-md ${!allowed ? 'bg-slate-500' : 'bg-indigo-600 hover:bg-indigo-700'} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
         onClick={showTransactionForm}
-        disabled={selectedTransactionIds.length > 0}
+        disabled={!allowed}
       >
         New Transaction
       </button>
       <button
         className={`text-white py-2 px-3 rounded-md ${!allowed ? 'bg-slate-500' : 'bg-indigo-600 hover:bg-indigo-700'} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500`}
         onClick={showOptionForm}
-        disabled={selectedOptionIds.length > 0}
+        disabled={!allowed}
       >
         New Option
       </button>
